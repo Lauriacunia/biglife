5030d79be5a8a9b28d191d905dc13ba1708eef3d
Laura Acuña <laura@blockinar.com>
2021-09-20 13:01:29 -0300

    Componentizo la vista ActivityContent

---
+++ src/containers/ActivityContentOthers.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>ActivityContentOthers</h1>
+</template>
--- src/views/ActivityContent.vue
+++ src/views/ActivityContent.vue
@@ -1,17 +1,26 @@
 <template>
   <div>
     <ActivityContentCard v-bind:activity="activity" />
+    <ActivityContentBenefits v-bind:activity="activity" />
+    <ActivityContentRules v-bind:activity="activity" />
+    <ActivityContentOthers v-bind:activity="activity" />
   </div>
 </template>
 
 <script>
-import ActivityContentCard from "../containers/ActivityContentCard.vue";
 import axios from "axios";
+import ActivityContentCard from "../containers/ActivityContentCard.vue";
+import ActivityContentBenefits from "../containers/ActivityContentBenefits.vue";
+import ActivityContentRules from "../containers/ActivityContentRules.vue";
+import ActivityContentOthers from "../containers/ActivityContentOthers.vue";
 
 export default {
   name: "ActivityContent",
   components: {
     ActivityContentCard,
+    ActivityContentBenefits,
+    ActivityContentRules,
+    ActivityContentOthers,
   },
 
   data() {
